a6a58caa09e10f5d1276e15eeb39fdc94cf6650e
hannah <mjko8903@gmail.com>
2025-08-21 13:58:14 +0900

    중간커밋

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
+import Home from "./pages/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>네이버 로그인</Button>
+    </div>
+  );
+}
